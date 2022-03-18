29d4f2e92c16f0c9e3bdd258584e89bba7e6b7ef
Rohit-ChauhanRC <rc672800@gmail.com>
2022-03-18 09:39:13 +0530

    Header finised

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Headers from './components/Headers';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Headers />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
